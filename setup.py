025aeec47a971bf7a336cfa3fb8feece929f21f2
ja-wie <74369302+ja-wie@users.noreply.github.com>
2021-01-21 13:59:53 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import json
+
+
+with open('metadata.json', encoding='utf-8') as fp:
+    metadata = json.load(fp)
+
+
+setup(
+    name="lexibank_servamalagasy",
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_servamalagasy"],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={"lexibank.dataset": ["servamalagasy=lexibank_servamalagasy:Dataset"]},
+    install_requires=["pylexibank>=2.1"],
+    extras_require={"test": ["pytest-cldf"]},
+)
