5c5d6ab67a70a7651b4858ece645f9dc0b901efc
ja-wie <74369302+ja-wie@users.noreply.github.com>
2021-01-21 14:57:42 +0100

    Delete lexibank_servamalagasy.py

--- lexibank_servamalagasy.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-from pylexibank.dataset import Dataset as BaseDataset 
-from pylexibank import Language, FormSpec
-from pylexibank import progressbar
-
-from clldutils.misc import slug
-import attr
